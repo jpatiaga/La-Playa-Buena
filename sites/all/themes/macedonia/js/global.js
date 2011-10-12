b52193a73035428d2a8c17dd5ae5483d4b009792
Juan Pablo Atiaga <jpatiaga@jobsity.com>
2011-10-11 23:06:36 -0500

    a

--- sites/all/themes/macedonia/js/global.js
+++ sites/all/themes/macedonia/js/global.js
@@ -1,4 +1,8 @@
 $ = jQuery;
 $(document).ready( function() {
+	// homepage slideshow
 	$('.homepage-slideshow ul').cycle();
+
+	// date fields datepicker
+	$('#edit-check-in, #edit-check-out').datepicker();
 });
